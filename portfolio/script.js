394715fdf56dc03f915972f2728ff82b518c2245
sagnikghosal <sagnikghosal.23@gmail.com>
2025-03-27 18:45:06 +0530

    Add files via upload

---
+++ portfolio/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.text', {
+  strings: ['an Engineer', 'a Data Analyst', 'a Frontend Developer'],
+  typeSpeed: 100,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true
+});
+
